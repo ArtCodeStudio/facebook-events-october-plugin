29f2448d7a63161bd8c836b035081bbe210515a9
daslicht <git@ansolas.de>
2020-10-13 04:31:44 -0400

    improved facebook login workflow

--- formwidgets/accesstokeninfo/assets/js/accesstokeninfo.js
+++ formwidgets/accesstokeninfo/assets/js/accesstokeninfo.js
@@ -1,17 +1,16 @@
 window.onload = function() {
-  var facebook_login_link = document.getElementById("facebook_login_link")
-  var facebook_login_url = document.getElementById("facebook_login_url")
-  var app_id_input = document.getElementsByName("Settings[app_id]")[0]
-  var app_secret_input = document.getElementsByName("Settings[app_secret]")[0]
-  var event_page_name_input = document.getElementsByName("Settings[event_page_name]")[0]
+  var facebook_login_link = document.getElementById("facebook_login_link");
+  var facebook_login_url = document.getElementById("facebook_login_url");
+  var app_id_input = document.getElementsByName("Settings[app_id]")[0];
+  var app_secret_input = document.getElementsByName("Settings[app_secret]")[0];
+  var event_page_name_input = document.getElementsByName("Settings[event_page_name]")[0];
 
-  function getLoginLink() {
+  function getLoginLink( callback ) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = () => {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
-          facebook_login_url.href = httpRequest.responseText;
-          facebook_login_link.style.opacity = 1;
+          callback(httpRequest.responseText);
         } else {
           alert('There was a problem with the request.');
         }
@@ -27,13 +26,27 @@ window.onload = function() {
         data: {redirect:0},
         success: function() {
             console.log('Finished!');
-            getLoginLink();
         }
       });
     } else {
       console.log('some inputs missing');
     }
   }
+
+  $('form').on('ajaxSuccess', function() {
+    $('form').request('onSave', {
+      data: {redirect:0},
+      success: function() {
+          getLoginLink( function(loginLink)
+          {
+            console.log('returned: ',loginLink)
+            window.location.href = loginLink;
+          });
+      }
+    });
+    console.log('Updated!');
+  })
+
   app_id_input.addEventListener('input', checkInput);
   app_secret_input.addEventListener('input', checkInput);
   event_page_name_input.addEventListener('input', checkInput);
