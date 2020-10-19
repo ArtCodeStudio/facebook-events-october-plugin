1504b1fe23ba01a67f072f7dc5f7b0f9b45c4c45
Moritz Raguschat <moritz@jumplink.me>
2020-10-19 18:46:15 +0700

    AccessToken Widget cleanup

--- formwidgets/accesstokeninfo/assets/js/accesstokeninfo.js
+++ formwidgets/accesstokeninfo/assets/js/accesstokeninfo.js
@@ -1,11 +1,5 @@
 window.onload = function() {
-  var facebook_login_link = document.getElementById("facebook_login_link");
-  var facebook_login_url = document.getElementById("facebook_login_url");
-  var app_id_input = document.getElementsByName("Settings[app_id]")[0];
-  var app_secret_input = document.getElementsByName("Settings[app_secret]")[0];
-  var event_page_name_input = document.getElementsByName("Settings[event_page_name]")[0];
-
-  function getLoginLink( callback ) {
+  function getLoginLink(callback) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = () => {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
@@ -22,12 +16,11 @@ window.onload = function() {
 
   $('form').on('ajaxSuccess', function() {
     $('form').request('onSave', {
-      data: {redirect:0},
+      data: {redirect: 0},
       success: function() {
-          getLoginLink( function(loginLink)
-          {
-            window.location.href = loginLink;
-          });
+        getLoginLink(function(loginLink) {
+          window.location.href = loginLink;
+        });
       }
     });
   })
